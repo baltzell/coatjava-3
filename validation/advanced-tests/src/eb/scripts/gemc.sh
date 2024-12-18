c992111b985dc357905674db6b9e7e44c5f6fc30
Nathan Baltzell <baltzell@gmx.com>
2024-12-18 16:27:19 -0500

    use an array

--- validation/advanced-tests/src/eb/scripts/gemc.sh
+++ validation/advanced-tests/src/eb/scripts/gemc.sh
@@ -5,21 +5,21 @@ usage() { echo "Usage: $0 [-g GEMC] [-n NEV] [-p PARTS] [-c GCARD]" 1>&2; exit $
 run=11
 gemc=5.10
 nevnts=100
-particles=electronproton
+particles=()
 
-while getopts "g:n:p:c:h" o
+while getopts "g:n:c:p:h" o
 do
-    case "${o}" in
+    case ${o} in
         g) gemc=${OPTARG} ;;
         n) nevents=${OPTARG} ;;
-        p) particles=${OPTARG} ;;
         c) gcard=${OPTARG} ;;
+        p) particles+=(${OPTARG}) ;;
         h) usage 0 ;;
         *) usage 1 ;;
     esac
 done
 
-if [ "$particles" == "all" ]
+if [ ${#particles[@]} -eq 0 ]
 then
     top=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
     for x in `awk '{print$1}' $top/list.txt`
@@ -34,7 +34,6 @@ else
         test -d clas12-config || git clone https://github.com/jeffersonlab/clas12-config
         gcard=clas12-config/gemc/$gemc/clas12-default.gcard 
     fi
-
     echo gemc \
     $gcard \
     -INPUT_GEN_FILE="LUND, $particles.txt" \
