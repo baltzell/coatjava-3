949edc62a943f417b1510eee1e29350661a46de7
Nathan Baltzell <baltzell@gmx.com>
2024-12-18 15:19:18 -0500

    merge scripts

--- validation/advanced-tests/src/eb/scripts/gemc.sh
+++ validation/advanced-tests/src/eb/scripts/gemc.sh
@@ -1,28 +1,49 @@
 #!/bin/sh
 
-if ! [ -e "$1.txt" ]
-then
-    echo Missing input file:  $1.txt
-    exit
-fi
-if [ -e "$1.evio" ] || [ -e "$1.hipo" ]
-then
-    echo Output file already exists:  ${1}.evio/hipo
-    exit
-fi
-
-v=5.10
-test -d clas12-config || git clone https://github.com/jeffersonlab/clas12-config
+usage() { echo "Usage: $0 [-g GEMC] [-n NEV] [-p PARTS] [-c GCARD]" 1>&2; exit $1; }
 
 run=11
-nEvents=1000
-gcard=clas12-config/gemc/$v/clas12-default.gcard 
+gemc=5.10
+nevnts=100
+particles=electronproton
 
-gemc \
+while getopts "g:n:p:c:h" o
+do
+    case "${o}" in
+        g) gemc=${OPTARG} ;;
+        n) nevents=${OPTARG} ;;
+        p) particles=${OPTARG} ;;
+        c) gcard=${OPTARG} ;;
+        h) usage 0 ;;
+        *) usage 1 ;;
+    esac
+done
+
+if [ "$particles" == "all" ]
+then
+    top=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+    for x in `awk '{print$1}' $top/list.txt`
+    do
+        $top/gemc.sh -p $x
+    done
+else 
+    ! [ -e "$particles.txt" ] && echo Missing input file:  $particles.txt && exit 2
+    [ -e "$particles.hipo" ] && echo Output file already exists:  $particles.hipo && exit 3
+    if [ -z ${gcard+x} ]
+    then
+        test -d clas12-config || git clone https://github.com/jeffersonlab/clas12-config
+        gcard=clas12-config/gemc/$gemc/clas12-default.gcard 
+    fi
+
+    echo gemc \
     $gcard \
-    -INPUT_GEN_FILE="LUND, $1.txt" \
-    -OUTPUT="hipo, $1.hipo" \
+    -INPUT_GEN_FILE="LUND, $particles.txt" \
+    -OUTPUT="hipo, $particles.hipo" \
     -RUNNO=$run \
     -USE_GUI=0 \
-    -N=$nEvents
-
+    -N=$nevents \
+    -SAVE_ALL_MOTHERS=1 \
+    -SKIPREJECTEDHITS=1 \
+    -INTEGRATEDRAW="*" \
+    -NGENP=50
+fi
