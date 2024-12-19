c822a4a6ff3c257e1c614f5b8948366aff8c55c2
Nathan Baltzell <baltzell@gmx.com>
2024-12-19 14:06:32 -0500

    now it works

--- validation/advanced-tests/src/eb/scripts/gemc.sh
+++ validation/advanced-tests/src/eb/scripts/gemc.sh
@@ -23,40 +23,42 @@ done
 
 if [ ${#particles[@]} -eq 0 ]
 then
-    pids=()
     top=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
     for x in $(awk '{print$1}' $top/list.txt)
     do
-        [ -z ${dryrun+x} ] && arg= || arg=-d
-        if [ -z ${threads+x} ]
-        then
-            $top/gemc.sh $arg -p $x
-        else
-            $top/gemc.sh $arg -p $x &
-            pids+=($!)
-        fi
-    done
-    wait
-else 
-    for p in "${particles[@]}"
-    do
-        ! [ -e "$p.txt" ] && echo Missing input file:  $p.txt && exit 2
-        [ -e "$p.hipo" ] && echo Output file already exists:  $p.hipo && exit 3
-        if [ -z ${gcard+x} ]
-        then
-            test -d clas12-config || git clone https://github.com/jeffersonlab/clas12-config
-            gcard=clas12-config/gemc/$gemc/clas12-default.gcard 
-        fi
-        $dryrun gemc \
-        $gcard \
-        -INPUT_GEN_FILE="LUND, $p.txt" \
-        -OUTPUT="hipo, $p.hipo" \
-        -RUNNO=$run \
-        -USE_GUI=0 \
-        -N=$nevents \
-        -SAVE_ALL_MOTHERS=1 \
-        -SKIPREJECTEDHITS=1 \
-        -INTEGRATEDRAW="*" \
-        -NGENP=50
+        particles+=($x)
     done
 fi
+
+if [ -z ${gcard+x} ]
+then
+    test -d clas12-config || git clone https://github.com/jeffersonlab/clas12-config
+    gcard=clas12-config/gemc/$gemc/clas12-default.gcard 
+fi
+
+for p in "${particles[@]}"
+do
+    ! [ -e "$p.txt" ] && echo Missing input file:  $p.txt && exit 2
+    [ -e "$p.hipo" ] && echo Output file already exists:  $p.hipo && exit 3
+    arg1="-RUNNO=$run -USE_GUI=0 -N=$nevents"
+    arg2='-SAVE_ALL_MOTHERS=1 -SKIPREJECTEDHITS=1 -INTEGRATEDRAW="*" -NGENP=50'
+    if [ -z ${threads+x} ]
+    then
+        set -x
+        $dryrun gemc \
+        $gcard $arg1 $arg2\
+        -INPUT_GEN_FILE="LUND, $p.txt" \
+        -OUTPUT="hipo, $p.hipo"
+        set +x
+    else
+        set -x
+        $dryrun gemc \
+        $gcard $arg1 $arg2\
+        -INPUT_GEN_FILE="LUND, $p.txt" \
+        -OUTPUT="hipo, $p.hipo" &
+        set +x
+    fi
+done
+
+wait
+
