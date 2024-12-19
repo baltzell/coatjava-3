f0a7cf535779bf95b12c0b9b45f9eac68755e7ba
Nathan Baltzell <baltzell@gmx.com>
2024-12-19 12:18:01 -0500

    actually loop

--- validation/advanced-tests/src/eb/scripts/gemc.sh
+++ validation/advanced-tests/src/eb/scripts/gemc.sh
@@ -7,7 +7,7 @@ gemc=5.10
 nevnts=100
 particles=()
 
-while getopts "g:n:c:p:th" o
+while getopts "g:n:c:p:tdh" o
 do
     case ${o} in
         g) gemc=${OPTARG} ;;
@@ -15,6 +15,7 @@ do
         c) gcard=${OPTARG} ;;
         p) particles+=(${OPTARG}) ;;
         t) threads=yes ;;
+        d) dryrun=echo ;;
         h) usage 0 ;;
         *) usage 1 ;;
     esac
@@ -22,33 +23,40 @@ done
 
 if [ ${#particles[@]} -eq 0 ]
 then
+    pids=()
     top=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
     for x in $(awk '{print$1}' $top/list.txt)
     do
+        [ -z ${dryrun+x} ] && arg= || arg=-d
         if [ -z ${threads+x} ]
         then
-            $top/gemc.sh -p $x
+            $top/gemc.sh $arg -p $x
         else
-            $top/gemc.sh -p $x &
+            $top/gemc.sh $arg -p $x &
+            pids+=($!)
         fi
     done
+    wait
 else 
-    ! [ -e "$particles.txt" ] && echo Missing input file:  $particles.txt && exit 2
-    [ -e "$particles.hipo" ] && echo Output file already exists:  $particles.hipo && exit 3
-    if [ -z ${gcard+x} ]
-    then
-        test -d clas12-config || git clone https://github.com/jeffersonlab/clas12-config
-        gcard=clas12-config/gemc/$gemc/clas12-default.gcard 
-    fi
-    gemc \
-    $gcard \
-    -INPUT_GEN_FILE="LUND, $particles.txt" \
-    -OUTPUT="hipo, $particles.hipo" \
-    -RUNNO=$run \
-    -USE_GUI=0 \
-    -N=$nevents \
-    -SAVE_ALL_MOTHERS=1 \
-    -SKIPREJECTEDHITS=1 \
-    -INTEGRATEDRAW="*" \
-    -NGENP=50
+    for p in "${particles[@]}"
+    do
+        ! [ -e "$p.txt" ] && echo Missing input file:  $p.txt && exit 2
+        [ -e "$p.hipo" ] && echo Output file already exists:  $p.hipo && exit 3
+        if [ -z ${gcard+x} ]
+        then
+            test -d clas12-config || git clone https://github.com/jeffersonlab/clas12-config
+            gcard=clas12-config/gemc/$gemc/clas12-default.gcard 
+        fi
+        $dryrun gemc \
+        $gcard \
+        -INPUT_GEN_FILE="LUND, $p.txt" \
+        -OUTPUT="hipo, $p.hipo" \
+        -RUNNO=$run \
+        -USE_GUI=0 \
+        -N=$nevents \
+        -SAVE_ALL_MOTHERS=1 \
+        -SKIPREJECTEDHITS=1 \
+        -INTEGRATEDRAW="*" \
+        -NGENP=50
+    done
 fi
